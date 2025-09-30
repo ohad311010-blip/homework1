ef07b6d39b8cbcc726bfe99451e1bf19d94fc693
ohad311010-blip <ohad311010@gmail.com>
2025-09-30 22:34:59 +0300

    sibit

--- Receiving a number.py
+++ Receiving a number.py
@@ -1,5 +1,12 @@
 def Receiving_a_number():
     number = int(input("Enter a number: "))
+    if number < 0:
+        print("Error: Please enter a non-negative integer.")
+    
+def sibit():
+    sibit=int(input("Enter sibit 1-8: "))
+    if sibit < 1 or sibit > 8:
+        print("Error: Please enter a number between 1 and 8.")
     
 def Decimal_to_binary_number(number):
     binary = format(number, '08b')
